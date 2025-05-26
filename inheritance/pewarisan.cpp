e133d2c5a410fe15f37f72d96a2692c59d5c7e6a
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-05-26 20:53:40 +0700

    membuat kelas orang

---
+++ inheritance/pewarisan.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang
+{
+    public:
+        string nama;
+
+        orang(string pNama) : nama(pNama)
+        {
+            cout << "Orang Dibuat\n" << endl;
+        }
+
+        ~orang()
+        {
+            cout << "Orang Dihapus\n" << endl;
+        }
+
+        int jumlah(int a, int b)
+        {
+            return a + b;
+        }
+};
+
+class pelajar : public orang
+{
+    public:
+        string sekolah;
+
+        pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah)
+        {
+            cout << "Pelajar Dibuat\n" << endl;
+        }
+
+        ~pelajar()
+        {
+            cout << "Pelajar Dihapus\n" << endl;
+        }
+
+        string perkenalan()
+        {
+            return "Halo, Nama saya " + nama + "dari sekolah " + sekolah + "\n";
+        }
+};
+
+int main()
+{
+    pelajar siswa1("Andi laksono", "SMAN 1 Bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+    return 0;
+}
