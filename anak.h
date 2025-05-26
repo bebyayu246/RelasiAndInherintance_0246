7c003b35ca787195d09c69b2bb4ee5cf376d2b47
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-05-26 18:30:53 +0700

    mengecek apakah nama anak tidak ada

--- anak.h
+++ anak.h
@@ -9,5 +9,7 @@ class anak {
             cout << "Anak \"" << nama << "\" ada\n ";
         }
         ~anak()
-      
+        {
+            cout << "Anak \"" << nama << "\" tidak ada\n";
+        }
 };
