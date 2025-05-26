b3594e40bda6e321a458c5776b5a37eeaaa6f409
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2025-05-26 20:54:49 +0700

    membuat include jantung & manusia

---
+++ komposisi/komposisi.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+#include "Jantung.h"
+#include "Manusia.h"
+
+int main() 
+{
+    manusia *varManusia = new manusia("Ipul Sagapunk");
+}
